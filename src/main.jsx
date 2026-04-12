34299a1742cc93751727b80001f4aba65b659996
serradequacions <serradequacions@gmail.com>
2026-04-12 13:42:34 +0200

    fix: force unregister old service workers

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Forçar desregistre de Service Workers vells
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(registrations => {
+    registrations.forEach(registration => registration.unregister())
+  })
+  caches.keys().then(keys => keys.forEach(key => caches.delete(key)))
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
