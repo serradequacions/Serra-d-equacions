2f0aaca2f201e26050792079efdee31382dc07dc
serradequacions <serradequacions@gmail.com>
2026-04-12 15:52:55 +0200

    fix: correct base path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Serra-d-equacions/app/',
+  base: '/Serra-d-equacions/',
   plugins: [
     react(),
   ]
