4a2401b137a33d9d96a956303571c9d2333abea8
serradequacions <serradequacions@gmail.com>
2026-04-12 16:19:58 +0200

    fix: rename SW to sw-v2.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/Serra-d-equacions/',
+  base: '/Serra-d-equacions/app/',
   plugins: [
     react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      filename: 'sw-v2.js',
+      includeAssets: ['favicon.ico', 'pwa-192x192.png', 'pwa-512x512.png'],
+      workbox: {
+        skipWaiting: true,
+        clientsClaim: true,
+        navigateFallback: null,
+        runtimeCaching: [],
+        globPatterns: ['**/*.{css,html,ico,png,svg}'],
+      },
+      manifest: {
+        name: "Serra d'Equacions",
+        short_name: "Serra d'Equacions",
+        theme_color: '#8fb382',
+        icons: [
+          { src: 'pwa-192x192.png', sizes: '192x192', type: 'image/png' },
+          { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png' }
+        ]
+      }
+    })
   ]
 })
