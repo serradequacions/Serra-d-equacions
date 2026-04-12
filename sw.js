f6709e39a53fee24a63dce0f29ca0b36493dfe7c
serradequacions <serradequacions@gmail.com>
2026-04-12 14:21:07 +0200

    fix: wipe all old files

--- sw.js
+++
@@ -1,1 +0,0 @@
-self.addEventListener('install', () => { self.skipWaiting(); }); self.addEventListener('activate', (e) => { e.waitUntil(caches.keys().then(k => Promise.all(k.map(c => caches.delete(c)))).then(() => self.clients.claim())); });
