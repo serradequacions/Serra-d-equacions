9b3953744bab9a46c62be6fef47998f236ef4b9a
serradequacions <serradequacions@gmail.com>
2026-04-12 13:55:56 +0200

    fix: remove PWA to break cache cycle

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   base: '/Serra-d-equacions/',
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'pwa-192x192.png', 'pwa-512x512.png'],
-      workbox: {
-        skipWaiting: true,
-        clientsClaim: true,
-        navigateFallback: null,
-        runtimeCaching: [],
-        globPatterns: ['**/*.{css,html,ico,png,svg}'],
-      },
-      manifest: {
-        name: "Serra d'Equacions",
-        short_name: "Serra d'Equacions",
-        theme_color: '#8fb382',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
   ]
 })
