11814cde25ae4c33ce8737051631141fd621be26
serradequacions <serradequacions@gmail.com>
2026-04-12 12:24:29 +0200

    fix: add clean vite config

---
+++ vite.config.js
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  base: '/Serra-d-equacions/',
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'pwa-192x192.png', 'pwa-512x512.png'],
+      workbox: {
+        skipWaiting: true,
+        clientsClaim: true,
+      },
+      manifest: {
+        name: "Serra d'Equacions",
+        short_name: "Serra d'Equacions",
+        theme_color: '#8fb382',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
+})
