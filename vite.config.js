9760364a3da20319add80a55048a72ca6800906f
serradequacions <serradequacions@gmail.com>
2026-04-12 17:03:47 +0200

    c

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/Serra-d-equacions/app-v2/',
+  base: '/Serra-d-equacions/app/',
   plugins: [
     react(),
     VitePWA({
